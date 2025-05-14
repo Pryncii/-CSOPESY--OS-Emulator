f1f1b04e4243e19b084017dfe4508abd08064e0e
RocketRoooiz <138289829+RocketRoooiz@users.noreply.github.com>
2025-05-15 00:54:06 +0800

    VS Setup and ASCII
    Made it so that you can run directly in visual studio, also added the ASCIIs and color for the header.

--- osEmulator.cpp
+++ osEmulator.cpp
@@ -24,19 +24,44 @@ void report_util(){
     cout << "report-util command recognized. Doing something.\n";
 }
 
+// feel free to change the colors
 void header(){
-    cout << "*------------------------------------------------*\n";
-    cout << "Hello, welcome to the CSOPESY emulator!\n";
-    cout << "Type 'exit' to quit, 'clear' to clear the screen\n";
-    cout << "commands: 'initialize', 'screen', 'scheduler-test', 'scheduler-stop', 'report-util'\n";
-    cout << "*------------------------------------------------*\n";
+    cout << "+=====================================================+\n";
+    cout << "|\x1B[48;5;109m                                                     \033[0m|\n";
+    cout << "|\x1B[48;5;109m     \x1B[38;5;66m _____ _____ _____ _____ _____ _____ __ __      \033[0m|\n";
+    cout << "|\x1B[48;5;109m     \x1B[38;5;66m\x1B[48;5;193m|     |   __|     |  _  |   __|   __|  |  |\x1B[48;5;109m     \033[0m|\n";
+    cout << "|\x1B[48;5;109m     \x1B[38;5;66m\x1B[48;5;229m|   --|__   |  |  |   __|   __|__   |_   _|\x1B[48;5;109m     \033[0m|\n";
+    cout << "|\x1B[48;5;109m     \x1B[38;5;66m\x1B[48;5;223m|_____|_____|_____|__|\x1B[48;5;109m  \x1B[38;5;66m\x1B[48;5;223m|_____|_____|\x1B[48;5;109m \x1B[38;5;66m\x1B[48;5;223m|_|\x1B[48;5;109m       \033[0m|\n";
+    cout << "|\x1B[48;5;109m \x1B[38;5;66m _____ _____     _____           _     _            \033[0m|\n";
+    cout << "|\x1B[48;5;109m \x1B[38;5;66m\x1B[48;5;193m|     |   __|\x1B[48;5;109m   \x1B[38;5;66m\x1B[48;5;193m|   __|\x1B[48;5;109m\x1B[38;5;66m_____ _ _\x1B[48;5;109m\x1B[38;5;66m\x1B[48;5;193m| |\x1B[48;5;109m\x1B[38;5;66m___\x1B[48;5;109m\x1B[38;5;66m\x1B[48;5;193m| |\x1B[48;5;109m\x1B[38;5;66m_ ___ ___\x1B[48;5;109m  \033[0m|\n";
+    cout << "|\x1B[48;5;109m \x1B[38;5;66m\x1B[48;5;229m|  |  |__   |\x1B[48;5;109m   \x1B[38;5;66m\x1B[48;5;229m|   __|     | | | | .'|  _| . |  _|\x1B[48;5;109m \033[0m|\n";
+    cout << "|\x1B[48;5;109m \x1B[38;5;66m\x1B[48;5;223m|_____|_____|\x1B[48;5;109m   \x1B[38;5;66m\x1B[48;5;223m|_____|_|_|_|___|_|__,|_|\x1B[48;5;109m \x1B[38;5;66m\x1B[48;5;223m|___|_|\x1B[48;5;109m   \033[0m|\n";
+    cout << "|\x1B[48;5;109m                                                     \033[0m|\n";
+    cout << "|\x1B[48;5;109m                                                     \033[0m|\n";
+    cout << "+-----------------------------------------------------+\n";
+    cout << "| Welcome! Here are the commands:                     |\n";
+    cout << "| -  'initialize', 'screen', 'scheduler-test',        |\n";
+    cout << "| -  'scheduler-stop', 'report-util', 'clear',        |\n";
+    cout << "| -  'exit'                                           |\n";
+    cout << "|                                                     |\n";
+    cout << "| By: CSOPESY S20 Group 8                             |\n";
+    cout << "| Buencamino, Chua, Ruiz, Seperidad                   |\n";
+    cout << "+=====================================================+\n";
+
+
+
+    //cout << "Hello, welcome to the CSOPESY emulator!\n";
+    //cout << "Type 'exit' to quit, 'clear' to clear the screen\n";
+    //cout << "commands: 'initialize', 'screen', 'scheduler-test', 'scheduler-stop', 'report-util'\n";
 }
 
 void clear(){
     #ifdef _WIN32
         system("cls"); // Windows
+        header();
     #else
         system("clear"); // Unix/Linux/MacOS
+        header()
     #endif
 }
 
@@ -44,8 +69,8 @@ int main(){
 
     string command;
     
+    header();
     do{
-        header();
         cout << "Enter command: ";
         getline(cin, command);
         if (command == "clear") {
