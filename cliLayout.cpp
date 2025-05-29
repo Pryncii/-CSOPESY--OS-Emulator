0bd509b96eff0ed3a9ae639ee64c26056e9de6b4
Prince Buencamino <princebuencamino@gmail.com>
2025-05-29 21:08:49 +0800

    nvidia layout

--- cliLayout.cpp
+++ cliLayout.cpp
@@ -2,21 +2,26 @@
 #include <iostream>
 #include <string>
 #include <map>
+#include <ctime>
+#include <unordered_map>
+#include "Console.h"
+#include <cstdlib>
 using namespace std;
-/*
-SetConsoleCursorPosition() – move cursor
 
-SetConsoleTextAttribute() – set text color
+std::unordered_map<std::string, Console> screens;
 
-FillConsoleOutputCharacter() – draw borders
 
-WriteConsoleOutputCharacter() – write strings at positions
-
-GetStdHandle(STD_OUTPUT_HANDLE) – get console handle
-*/
 void SetCursorPosition(int x, int y) {
     COORD coord = {(SHORT)x, (SHORT)y};
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+int computeSpace(int width, string string1, string string2){
+    return width - string1.length() - string2.length();
+}
+
+void drawLine(int innerWidth, int innerWidth2, string string1, string string2, string string3, string string4, string string5){
+     cout << "|" << string1 << string(computeSpace(innerWidth, string1, string2), ' ') << string2 << "|" << string3 << string(computeSpace(innerWidth2, string3, string4), ' ') << string4 << "|" << string5 << string(innerWidth2 + 1 - string5.length(), ' ') << "|";
 }
 
 void DrawBox(int x, int y, int width, int height) {
@@ -35,106 +40,7 @@ void DrawBox(int x, int y, int width, int height) {
     string memoryUsage = "Memory-Usage";
     string volatileUnc = " Volatile Uncorr. ECC";
     string gpuUtil = " GPU-Util Compute M.";
-    string migM = "MIG M. ";
-
-    string nvidiaGforce = "   0 NVIDIA GeForce GTX 1080";
-    string wddm = "WDDM ";
-    string busVal = "00000000:26:00.0";
-    string dispVal = "On";
-    string nA = "N/A";
-    string fanTempPerfVal = "28%  37C    P8";
-    string pwrUsageCapVal = "11W / 180W";
-    string memoryUsageVal = "701MiB /   8192MiB";
-
-
-    std::map<string, string> headers = {
-        {"header1", " NVIDIA-SMI 551.86"},
-        {"header2", "Driver Version: 551.86"},
-        {"header3", "CUDA Version 12.4 "},
-        {"gpuName", "GPU Name"},
-        {"tccWddm", "TCC/WDDM "},
-        {"fanTempPerf", "Fan Temp  Perf"},
-        {"pwrUsageCap", "Pwr: Usage/Cap"},
-        {"busID", "Bus-Id"},
-        {"dispA", "Disp.A"},
-        {"memoryUsage", "Memory-Usage"},
-        {"volatileUnc", " Volatile Uncorr. ECC"},
-        {"gpuUtil", " GPU-Util Compute M."},
-        {"migM", "MIG M. "}
-    };
-
-    for (int i = 1; i < height - 1; ++i) {
-        SetCursorPosition(x, y + i);
-        if(i == height/4){
-            cout << "|" << string(width - 2, '-')  << "|";
-        } else if (i < height/4) {
-            int innerWidth = width - 2;
-            int leftLen = header1.length();
-            int centerLen = header2.length();
-            int rightLen = header3.length();
-
-            int spaceBetweenLeftAndCenter = (innerWidth - leftLen - centerLen - rightLen) / 2;
-            int spaceBetweenCenterAndRight = innerWidth - leftLen - centerLen - rightLen - spaceBetweenLeftAndCenter;
-
-            cout << "|"
-                      << header1
-                      << string(spaceBetweenLeftAndCenter, ' ')
-                      << header2
-                      << string(spaceBetweenCenterAndRight, ' ')
-                      << header3
-                      << "|";
-        } else if (i == height/2 + 1){
-            cout << "|" << string(width - 2, '=')  << "|";
-        } else {
-        if(i == 3){
-            int innerWidth = width * 0.4 - 1;
-            int innerWidth2= width * 0.3 - 1;
-            int leftLen1 = gpuName.length();
-            int rightLen1  = tccWddm.length();
-            int spaceInBetween1 = innerWidth - leftLen1 - rightLen1;
-            int leftLen2 = busID.length();
-            int rightLen2  = dispA.length();
-            int spaceInBetween2 = innerWidth2 - leftLen2 - rightLen2;
-            cout << "|" << gpuName << string(spaceInBetween1, ' ') << tccWddm << "|" << busID << string(spaceInBetween2, ' ') << dispA << "|" << volatileUnc << string(innerWidth2 + 1 - volatileUnc.length(), ' ') << "|";
-        } else if (i == 4){
-            int innerWidth = width * 0.4 - 1;
-            int innerWidth2= width * 0.3 - 1;
-            int leftLen1 = fanTempPerf.length();
-            int rightLen1  = pwrUsageCap.length();
-            int spaceInBetween1 = innerWidth - leftLen1 - rightLen1;
-            int len2 = memoryUsage.length();
-            int spaceInBetween2 = innerWidth2 - len2;
-            cout << "|" << fanTempPerf << string(spaceInBetween1, ' ') << pwrUsageCap << "|" << string(spaceInBetween2, ' ') << memoryUsage << "|" << gpuUtil << string(innerWidth2 + 1 - gpuUtil.length(), ' ') << "|";
-        } else if (i == 5) {
-            cout << "|" << string(width * 0.4 - 1, ' ') << "|" << string(width * 0.3 - 1, ' ') << "|" << string(width * 0.3 - 1 - migM.length(), ' ') << migM  << "|";
-        }
-        else{
-            cout << "|" << string(width * 0.4 - 1, ' ') << "|" << string(width * 0.3 - 1, ' ') << "|" << string(width * 0.3, ' ') << "|";
-        }
-    }
-
-    SetCursorPosition(x, y + height - 1);
-    cout << "+" << string(width - 2, '-') << "+";
-    }
-}
-
-void DrawBox2(int x, int y, int width, int height) {
-    SetCursorPosition(x, y);
-    cout << "+" << string(width - 2, '-') << "+";
-    // header information
-    string header1 = " NVIDIA-SMI 551.86";
-    string header2 = "Driver Version: 551.86";
-    string header3 = "CUDA Version 12.4 ";
-    string gpuName = "GPU Name";
-    string tccWddm = "TCC/WDDM ";
-    string fanTempPerf = "Fan Temp  Perf";
-    string pwrUsageCap = "Pwr: Usage/Cap";
-    string busID = "Bus-Id";
-    string dispA = "Disp.A";
-    string memoryUsage = "Memory-Usage";
-    string volatileUnc = " Volatile Uncorr. ECC";
-    string gpuUtil = " GPU-Util Compute M.";
-    string migM = "MIG M. ";
+    string migM = " MIG M. ";
 
     string nvidiaGforce = "   0 NVIDIA GeForce GTX 1080";
     string wddm = "WDDM ";
@@ -164,34 +70,85 @@ void DrawBox2(int x, int y, int width, int height) {
             cout << "|" << string(width - 2, '=')  << "|";
         } else {
         if(i == 3){
-            spaceInBetween1 = innerWidth - gpuName.length() - tccWddm.length();
-            spaceInBetween2 = innerWidth2 - busID.length() - dispA.length();
-            cout << "|" << gpuName << string(spaceInBetween1, ' ') << tccWddm << "|" << busID << string(spaceInBetween2, ' ') << dispA << "|" << volatileUnc << string(innerWidth2 + 1 - volatileUnc.length(), ' ') << "|";
-        } else if (i == 4){
-            spaceInBetween1 = innerWidth - fanTempPerf.length() - pwrUsageCap.length();
-            spaceInBetween2 = innerWidth2 - memoryUsage.length();
-            cout << "|" << fanTempPerf << string(spaceInBetween1, ' ') << pwrUsageCap << "|" << string(spaceInBetween2, ' ') << memoryUsage << "|" << gpuUtil << string(innerWidth2 + 1 - gpuUtil.length(), ' ') << "|";
+            drawLine(innerWidth, innerWidth2, gpuName, tccWddm, busID, dispA, volatileUnc);
+        } else if (i == 4) {
+            drawLine(innerWidth, innerWidth2, fanTempPerf, pwrUsageCap, "", memoryUsage, gpuUtil);
         } else if (i == 5) {
-            cout << "|" << string(width * 0.4 - 1, ' ') << "|" << string(width * 0.3 - 1, ' ') << "|" << string(width * 0.3 - 1 - migM.length(), ' ') << migM  << "|";
+            drawLine(innerWidth, innerWidth2, "", "", "", "", migM);
         } else if (i == 7) {
-            spaceInBetween1 = innerWidth - nvidiaGforce.length(); - wddm.length();
-            spaceInBetween2 = innerWidth2 - busVal.length(); - dispVal.length();
-            cout << "|" << nvidiaGforce << string(spaceInBetween1, ' ') << wddm << "|"  << busVal << string(spaceInBetween2, ' ') << dispVal << "|" << string(innerWidth2 + 1 - gpuUtil.length(), ' ') << nA << "|";
+            drawLine(innerWidth, innerWidth2, nvidiaGforce, wddm, busVal, dispVal, nA);
+        } else if (i == 8) {
+            drawLine(innerWidth, innerWidth2, fanTempPerfVal, pwrUsageCap, "", memoryUsageVal, gpuUtilCompute);
+        } else if (i == 9) {
+            drawLine(innerWidth, innerWidth2, "", "", "", "", nA);
         }
         else{
             cout << "|" << string(width * 0.4 - 1, ' ') << "|" << string(width * 0.3 - 1, ' ') << "|" << string(width * 0.3, ' ') << "|";
         }
     }
-
+    }
     SetCursorPosition(x, y + height - 1);
     cout << "+" << string(width - 2, '-') << "+";
+}
+
+void printProcesses(int x, int y, int width, int height, int i){
+    int spacing = 3;
+    
+    for (const auto& pair : screens) {
+        SetCursorPosition(x, y + i);
+        srand(static_cast<unsigned int>(time(0))); // Seed the random number generator
+        int randomNum = rand() % 9000 + 1000; // Generates a number between 1000 and 9999
+        string line = "|" + string(spacing + 1, ' ') + '0' + string(spacing, ' ') + "N/A" +
+              string(spacing - 1, ' ') + "N/A" + string(spacing + 1, ' ') +
+              to_string(randomNum) + string(spacing, ' ') + "C+G" +
+              string(spacing + 1, ' ') + pair.second.getName();
+        cout << line << string(width - 10 - line.length(), ' ') << "N/A" << string(spacing * 2, ' ') << "|";
+        ++i;
     }
+    SetCursorPosition(x, y + screens.size() + height - 1);
+    cout << "+" << string(width - 2, '-') << "+";
+   
+}
+void DrawBox2(int x, int y, int width, int height) {
+    SetCursorPosition(x, y);
+    cout << "+" << string(width - 2, '-') << "+";
+    // header information
+    string processHeader = " Processes:";
+    string leftHeader1 = "  GPU   GI   CI      PID   Type   Process name";
+    string rightHeader1 = "GPU Memory ";
+    string leftHeader2 = "         ID   ID";
+    string rightHeader2 = "Usage ";
+ 
+    int i;
+    for (i = 1; i < height - 1; ++i) {
+        SetCursorPosition(x, y + i);
+
+        if (i == 1){
+            cout << "|" << processHeader << string(width - 2 - processHeader.length(), ' ') << "|";
+        } else if (i == 2){
+            cout << "|" << leftHeader1 << string(width - 2 - leftHeader1.length() - rightHeader1.length(), ' ') << rightHeader1 << "|";
+        } else if(i == 3){
+            cout << "|" << leftHeader2 << string(width - 2 - leftHeader2.length() - rightHeader2.length(), ' ') << rightHeader2 << "|";
+        } else if (i == 4){
+            cout << "|" << string(width - 2, '=')  << "|";
+        } 
+    }
+
+    printProcesses(x, y, width, height, i);
+
 }
 
 
 int main() {
     system("cls");
-    DrawBox(1, 5, 80, 11);
+    for(int i = 0; i < 5; i++){
+        string screenName = "screen" +  to_string(i);
+        Console temp(screenName);
+        screens.insert({ screenName, temp });
+        cout << screens[screenName].getName();
+    }
+    DrawBox(1, 5, 90, 11);
+    DrawBox2(1, 16, 90, 6);
 
     SetCursorPosition(7, 4);
     SetCursorPosition(0, 0);
