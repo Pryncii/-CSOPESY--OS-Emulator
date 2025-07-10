409df33715529723ba4c5438751dfee63bcff6c2
Abigail Seperidad <159636961+abigailseperidad@users.noreply.github.com>
2025-07-10 14:12:12 +0800

    fma ndg implementation

--- FlatMemoryAllocator.cpp
+++ FlatMemoryAllocator.cpp
@@ -3,8 +3,37 @@
 #include <memory>
 
 FlatMemoryAllocator::FlatMemoryAllocator(uint16_t size) {
-	memory.reserve(size);
+	maxSize = size;
+	allocatedSize = 0;
+	memory.reserve(maxSize);
 	initializeMemory();
+}
+
+FlatMemoryAllocator::~FlatMemoryAllocator() {
+	memory.clear();
+}
+
+
+void* FlatMemoryAllocator::Allocate(uint16_t size) {
+	for(uint16_t i = 0; i < maxSize - size + 1; ++i) {
+		if(!allocationMap[i] && canAllocateAt(i, size)) {
+			allocateAt(i, size);
+			return &memory[i];
+		}
+	}
+
+	return nullptr;
+}
+
+void FlatMemoryAllocator::Deallocate(void* ptr) {
+	uint16_t index = static_cast<char*>(ptr) - &memory[0];
+	if(allocationMap[index]) {
+		deallocateAt(index);
+	}
+}
+
+string FlatMemoryAllocator::visualizeMemory() const {
+	return string(memory.begin(), memory.end());
 }
 
 void FlatMemoryAllocator::initializeMemory() {
@@ -13,3 +42,15 @@ void FlatMemoryAllocator::initializeMemory() {
 	fill(allocationMap.begin(), allocationMap.end(), false);
 }
 
+bool FlatMemoryAllocator::canAllocateAt(uint16_t index, uint16_t size) const {
+	return (index + size <= maxSize);
+}
+
+void FlatMemoryAllocator::allocateAt(uint16_t index, uint16_t size) {
+	fill(allocationMap.begin() + index, allocationMap.begin() + index + size, true);
+	allocatedSize += size;
+}
+
+void FlatMemoryAllocator::deallocateAt(uint16_t index) {
+	allocationMap[index] = false;
+}
